8b13c61659673c6d69b8f79f16c57818627ac608
Tushar Velingkar <Tushar.Velingkar@ibm.com>
2024-03-08 01:20:32 +0530

    Task 5: Create API to delete todo by id

--- routers/todos.py
+++ routers/todos.py
@@ -46,3 +46,13 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found.")
+
+
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found.")
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+
+    db.commit()
